c91014972e6109217fb77f87a5371890edd38706
Luewngier <g217881111@gmail.com>
2024-01-01 23:48:32 +0800

    Try list comprehension pattern

---
+++ main.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3]
+# Original way:
+# new_list = []
+
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+
+# List Comprehension:
+new_list = [n + 1 for n in numbers]
