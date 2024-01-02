aa209b246273824994136e3c79c9112a1e8b49fa
Luewngier <g217881111@gmail.com>
2024-01-02 23:34:41 +0800

    Make use of conditional comprehension

--- main.py
+++ main.py
@@ -9,6 +9,9 @@
 # List Comprehension:
 # new_list = [n + 1 for n in numbers]
 
-doubled_numbers = [n * 2 for n in range(1, 5)]
+# doubled_numbers = [n * 2 for n in range(1, 5)]
 
-print(doubled_numbers)
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+
+print(short_names)
