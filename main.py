c716bec33699367fc75937a891cde35c82059503
Luewngier <g217881111@gmail.com>
2024-01-05 23:53:24 +0800

    Make use of dictionary comprehension

--- main.py
+++ main.py
@@ -12,8 +12,14 @@
 # doubled_numbers = [n * 2 for n in range(1, 5)]
 
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-short_names = [name for name in names if len(name) < 5]
-long_names = [name.upper() for name in names if len(name) > 4]
+# short_names = [name for name in names if len(name) < 5]
+# long_names = [name.upper() for name in names if len(name) > 4]
 
-print(short_names)
-print(long_names)
+# print(short_names)
+# print(long_names)
+
+import random
+
+student_scores = {student: random.randint(1, 100) for student in names}
+
+print(student_scores)
