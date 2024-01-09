4b4fe5f1230f17b14384835e0a9d979cf8f04257
Luewngier <g217881111@gmail.com>
2024-01-09 23:11:23 +0800

    Create new dict from existing dict by dictionary comprehension

--- main.py
+++ main.py
@@ -11,15 +11,28 @@
 
 # doubled_numbers = [n * 2 for n in range(1, 5)]
 
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 # short_names = [name for name in names if len(name) < 5]
 # long_names = [name.upper() for name in names if len(name) > 4]
 
 # print(short_names)
 # print(long_names)
 
-import random
+# import random
 
-student_scores = {student: random.randint(1, 100) for student in names}
+# student_scores = {student: random.randint(1, 100) for student in names}
 
-print(student_scores)
+scores_dict = {
+    "Alex": 14,
+    "Beth": 83,
+    "Caroline": 56,
+    "Dave": 65,
+    "Eleanor": 63,
+    "Freddie": 38,
+}
+
+passed_students = {
+    student: score for (student, score) in scores_dict.items() if score >= 60
+}
+
+print(passed_students)
